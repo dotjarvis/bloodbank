8e0b5a5025689bdc77234caea6c59164bc8a9631
dotjarvis <johnsonmakings@gmail.com>
2023-12-31 19:55:25 +0300

    Replaced all * imports for optimization

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import *
+from .models import User, Profile, Blood_Request, Donation_Criteria_Form, DonationCriteriaQuestions, HospitalAddress, Donation
+
 from django.contrib.auth.admin import UserAdmin
 
 admin.site.register(Profile)
