858a7c3dcdbc2d311d8cfa51984eeb5cc5351337
jarvisthedev <johnsonmakings@gmail.com>
2024-06-30 23:06:43 +0300

    Attemp to connect to postgreSQL

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -117,10 +117,21 @@ WSGI_APPLICATION = 'bloodbank.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bloodbank',
+        'USER': 'jarvis',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
